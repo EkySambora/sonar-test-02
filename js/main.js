156ecc3ff35868ab6be2117aa094758db5596f69
Eky Umroh.com <ekyzulianto@umroh.com>
2020-04-28 09:49:08 +0700

    rapih2

--- js/main.js
+++ js/main.js
@@ -5,37 +5,39 @@ let valTab1 = document.querySelector('.tab-1');
 let valTab2 = document.querySelector('.tab-2');
 let pseudo = document.querySelector('.pseudo');
 let images = document.querySelectorAll('.image img');
+let indicator = document.querySelectorAll('.indicator span');
 
-coverAll.style.display = 'none'
-tabs[0].classList.add('active')
-valTab2.style.display = 'none'
+coverAll.style.display = 'none';
+tabs[0].classList.add('active');
+valTab2.style.display = 'none';
 
 tabs.forEach(tab => tab.addEventListener('click', activeTab));
 
 showPopover.addEventListener("click", function(){
-    coverAll.style.display = 'flex'
+    coverAll.style.display = 'flex';
 })
 
 pseudo.addEventListener("click", function() {
-    coverAll.style.display = 'none'
+    coverAll.style.display = 'none';
 }) 
 
 function activeTab(e){
     tabs.forEach(tab=>tab.classList.remove('active'))
     e.target.classList.toggle('active');
 
-    let dataSet = e.target.dataset.tab
+    let dataSet = e.target.dataset.tab;
+
     if(dataSet == 'two'){
-        valTab2.style.display = 'block'
-        valTab1.style.display = 'none'
+        valTab2.style.display = 'block';
+        valTab1.style.display = 'none';
     } else {
-        valTab2.style.display = 'none'
-        valTab1.style.display = 'block'
+        valTab2.style.display = 'none';
+        valTab1.style.display = 'block';
     }
 }
 
-images[1].style.display = "none"
-images[2].style.display = "none"
+images[1].style.display = "none";
+images[2].style.display = "none";
 
 //slider
 
@@ -43,27 +45,27 @@ var counter = 0;
 var newCountdown = setInterval(function(){
   console.log(counter);
   counter++
-  if(counter === 0) {
-    images[0].style.display = "block"
-    images[1].style.display = "none"
-    images[2].style.display = "none"
-  }
-  if(counter === 1) {
-    images[0].style.display = "none"
-    images[1].style.display = "block"
-    images[2].style.display = "none"
-}
-if(counter === 2) {
-    images[2].style.height = "300px"
-    images[0].style.display = "none"
-    images[1].style.display = "none"
-    images[2].style.display = "block"
-  }
-  if(counter === 3) {
-    images[0].style.display = "block"
-    images[1].style.display = "none"
-    images[2].style.display = "none"
-    counter = 0
-    // clearInterval(newCountdown);
-  }
+    if(counter === 0) {
+        images[0].style.display = "block";
+        images[1].style.display = "none";
+        images[2].style.display = "none";
+    }
+    else if(counter === 1) {
+        images[0].style.display = "none";
+        images[1].style.display = "block";
+        images[2].style.display = "none";
+    }
+    else if(counter === 2) {
+        images[2].style.height = "300px";
+        images[0].style.display = "none";
+        images[1].style.display = "none";
+        images[2].style.display = "block";
+    }
+    else if(counter === 3) {
+        images[0].style.display = "block";
+        images[1].style.display = "none";
+        images[2].style.display = "none";
+        counter = 0
+        // clearInterval(newCountdown);
+    }
 }, 3000);
