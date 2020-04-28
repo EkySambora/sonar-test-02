c8e7054ef10dc335d28e228d95f65ef7f9984638
Eky Umroh.com <ekyzulianto@umroh.com>
2020-04-28 09:38:29 +0700

    add slider

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
-let coverAll = document.querySelector('.cover-all')
-let showPopover = document.querySelector('#showPopover')
-let tabs = document.querySelectorAll('.tabs')
-let valTab1 = document.querySelector('.tab-1')
-let valTab2 = document.querySelector('.tab-2')
-let pseudo = document.querySelector('.pseudo')
+let coverAll = document.querySelector('.cover-all');
+let showPopover = document.querySelector('#showPopover');
+let tabs = document.querySelectorAll('.tabs');
+let valTab1 = document.querySelector('.tab-1');
+let valTab2 = document.querySelector('.tab-2');
+let pseudo = document.querySelector('.pseudo');
+let images = document.querySelectorAll('.image img');
 
 coverAll.style.display = 'none'
 tabs[0].classList.add('active')
@@ -14,7 +15,6 @@ tabs.forEach(tab => tab.addEventListener('click', activeTab));
 showPopover.addEventListener("click", function(){
     coverAll.style.display = 'flex'
 })
-
 
 pseudo.addEventListener("click", function() {
     coverAll.style.display = 'none'
@@ -32,5 +32,38 @@ function activeTab(e){
         valTab2.style.display = 'none'
         valTab1.style.display = 'block'
     }
-
 }
+
+images[1].style.display = "none"
+images[2].style.display = "none"
+
+//slider
+
+var counter = 0;
+var newCountdown = setInterval(function(){
+  console.log(counter);
+  counter++
+  if(counter === 0) {
+    images[0].style.display = "block"
+    images[1].style.display = "none"
+    images[2].style.display = "none"
+  }
+  if(counter === 1) {
+    images[0].style.display = "none"
+    images[1].style.display = "block"
+    images[2].style.display = "none"
+}
+if(counter === 2) {
+    images[2].style.height = "300px"
+    images[0].style.display = "none"
+    images[1].style.display = "none"
+    images[2].style.display = "block"
+  }
+  if(counter === 3) {
+    images[0].style.display = "block"
+    images[1].style.display = "none"
+    images[2].style.display = "none"
+    counter = 0
+    // clearInterval(newCountdown);
+  }
+}, 3000);
